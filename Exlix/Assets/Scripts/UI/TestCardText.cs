b7e890a0fe94d9eb83300d8ff80b1984d644bd07
ysh4267 <ysh4267@gmail.com>
2021-05-22 20:21:12 +0900

    테스트용 카드 및 게임매니져 메소드명 변경
    테스트용 카드 및 게임매니져 메소드명 변경

---
+++ Exlix/Assets/Scripts/UI/TestCardText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestCardText : MonoBehaviour {
+	CardDataTemplate cardData;
+	[SerializeField] int cardNum;
+
+	[SerializeField] Text property;
+	[SerializeField] Text cost;
+	[SerializeField] Text name;
+	[SerializeField] Text explain;
+
+	// Start is called before the first frame update
+	void Start() {
+		cardData = GameObject.Find("GameManager").GetComponent<GameManager>().CardParser.DataList[cardNum];
+		property.text = cardData.Property;
+		cost.text = cardData.Cost.ToString();
+		name.text = cardData.Name;
+		cardData.Explanation.Replace("다음", "asd");
+		explain.text = cardData.Explanation;
+		
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
