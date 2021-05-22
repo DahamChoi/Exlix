ea4c67c703061bed0442e53ea3750ca53c6bef8f
DahamChoi <rorigu32@gmail.com>
2021-05-22 14:40:12 +0900

    battle structure

---
+++ Exlix/Assets/Scripts/Battle/CardEffect.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardEffect {
+
+    static public void RunRoutine(CardDataTemplate DataTemplate, BattlePlayer Player, List<BattleEnemy> Target) {
+                
+    }
+}
