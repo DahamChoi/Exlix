70e958db87515f5210f8e449b69590eb93cf0268
woohyun95 <diddngus77@naver.com>
2021-06-04 19:06:13 +0900

    scenemanager in progress

--- Exlix/Assets/Scripts/UI/MoveScene.cs
+++ Exlix/Assets/Scripts/UI/MoveScene.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MoveScene : MonoBehaviour
 {
+    const int MainScreen = 0;
+    const int CreateCharacter = 1;
+    const int SelectDeck = 2;
+    Scene previousScene;
+    public void MoveToPreviousScene() {
+        SceneManager.LoadScene(previousScene.buildIndex);
+    }
     public void MoveToCreateCharacter() {
-        SceneManager.LoadScene("CreateCharacter");
+        SceneManager.LoadScene(CreateCharacter);
     }
     
     public void MoveToMainScene() {
-        SceneManager.LoadScene("MainScreen");
+        SceneManager.LoadScene(MainScreen);
     }
+
+    public void MoveToDeckSelectScene() {
+        SceneManager.LoadScene(SelectDeck);
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        previousScene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
