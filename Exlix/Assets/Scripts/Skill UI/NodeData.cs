d1974091045b454b622860bc0616c30ec62bff85
YangJiHoon <ysh4267@gmail.com>
2021-06-28 16:08:35 +0900

    Skill Prototype

---
+++ Exlix/Assets/Scripts/Skill UI/NodeData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NodeData : MonoBehaviour {
+    [SerializeField] NodeData pNode;
+    [SerializeField] Image nodeImage;
+    public int nodeId;
+    public bool isActivated;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!isActivated) {
+            nodeImage.color = new Color(75, 75, 75);
+        }
+        else if (!isActivated) {
+            nodeImage.color = new Color(255, 255, 255);
+        }
+        CheckPNode();
+    }
+
+    void CheckPNode() {
+        if (pNode == null) return;
+        if (!pNode.isActivated) isActivated = false;
+    }
+}
---
+++ Exlix/Assets/Scripts/Skill UI/SkillInfoPopup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillInfoPopup : MonoBehaviour
+{
+    [SerializeField] GameObject skillInfoWindow;
+    [SerializeField] Text Name;
+    [SerializeField] Text Explain;
+
+    NodeData nodeData;
+
+    // Start is called before the first frame update
+    void Start() {
+        nodeData = gameObject.GetComponent<NodeData>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void PopupSkillInfo() {
+        skillInfoWindow.SetActive(true);
+        Name.text = "Test Skill";
+        Explain.text = "Test Skill Explain";
+    }
+}
