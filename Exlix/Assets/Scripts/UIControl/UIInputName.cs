63e59fb5cad575f93403bd4019744c59409459ef
leejaebok96 <leejaebok96@gmail.com>
2021-06-30 13:54:22 +0900

    Character Portrait 기능
    Character Portrait 기능

---
+++ Exlix/Assets/Scripts/UIControl/UIInputName.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInputName : MonoBehaviour
+{
+    public GameObject InputNameScreen;
+    public GameObject InputNameCloser;
+    public Text InputNameText;
+    public Text OutpuNameText;
+    public List<string> RandomNames;
+
+    string name;
+
+    public void OpenInputNameScreen()
+    {
+        if (InputNameScreen.activeSelf == true)
+        {
+            InputNameScreen.SetActive(false);
+            InputNameCloser.SetActive(false);
+        }
+        else
+        {
+            InputNameScreen.SetActive(true);
+            InputNameCloser.SetActive(true);
+        }
+    }
+    public void CloseInputNameScreen()
+    {
+        InputNameScreen.SetActive(false);
+        InputNameCloser.SetActive(false);
+    }
+
+    public void ConfirmName()
+    {
+        name = InputNameText.text;
+        OutpuNameText.text = name;
+    }
+
+
+    public void InputRandomName()
+    {
+        int nameNum = Random.Range(0, RandomNames.Count);
+        OutpuNameText.text = RandomNames[nameNum];
+    }
+}
---
+++ Exlix/Assets/Scripts/UIControl/UIPortrait.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPortrait : MonoBehaviour
+{
+    public GameObject PortraitPopup;
+    public GameObject PortraitPopupCloser;
+
+    public void OpenPortraitScreen()
+    {
+        if (PortraitPopup.activeSelf == true)
+        {
+            PortraitPopup.SetActive(false);
+            PortraitPopupCloser.SetActive(false);
+        }
+        else
+        {
+            PortraitPopup.SetActive(true);
+            PortraitPopupCloser.SetActive(true);
+        }
+    }
+    public void ClosePortraitScreen()
+    {
+        PortraitPopup.SetActive(false);
+        PortraitPopupCloser.SetActive(false);
+    }
+
+    public void ConfirmPortrait()
+    {
+        PortraitPopup.SetActive(false);
+        PortraitPopupCloser.SetActive(false);
+    }
+
+    public void SelectPortrait()
+    {
+
+    }
+}
