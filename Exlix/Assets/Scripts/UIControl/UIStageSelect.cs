f2e915e36b417b596a36638fb9ab381fdfd9201a
woohyun95 <diddngus77@naver.com>
2021-06-30 15:58:37 +0900

    구역선택/구역내부씬 UI 배치

---
+++ Exlix/Assets/Scripts/UIControl/UIStageSelect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStageSelect : MonoBehaviour
+{
+    public GameObject PopupScreen;
+    public void OpenStagePopup() {
+        PopupScreen.SetActive(true);
+    }
+    public void CloseStagePopup() {
+        PopupScreen.SetActive(false);
+    }
+}
