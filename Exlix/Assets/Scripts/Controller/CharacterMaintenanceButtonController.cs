b93c0bcddf2f5685b067386a3f2386e2a414e163
leejaebok96 <leejaebok96@gmail.com>
2021-07-01 14:38:26 +0900

    CharacterMaintenanceScene
    CharacterMaintenanceScene

---
+++ Exlix/Assets/Scripts/Controller/CharacterMaintenanceButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMaintenanceButtonController : MonoBehaviour
+{
+    [SerializeField] Button EquipmentButton;
+    [SerializeField] Button SkillButton;
+    [SerializeField] Button NowEquipment;
+    [SerializeField] Button NowSkill;
+    [SerializeField] Button DeckButton;
+    [SerializeField] Button StartButton;
+
+    [SerializeField] SceneState _SceneState;
+
+    void Start()
+    {
+        EquipmentButton.onClick.AddListener(() => {
+            _SceneState._SceneStateHandler.ProcessEvent(GameStateMachine.TRIGGER.MAIN_TO_EQUIPMENT);
+        });
+
+        SkillButton.onClick.AddListener(() => {
+            _SceneState._SceneStateHandler.ProcessEvent(GameStateMachine.TRIGGER.MAIN_TO_SKILL);
+        });
+
+        NowEquipment.onClick.AddListener(() => {
+            _SceneState._SceneStateHandler.ProcessEvent(GameStateMachine.TRIGGER.MAIN_TO_EQUIPMENT);
+        });
+
+        NowSkill.onClick.AddListener(() => {
+            _SceneState._SceneStateHandler.ProcessEvent(GameStateMachine.TRIGGER.MAIN_TO_SKILL);
+        });
+    }
+}
---
+++ Exlix/Assets/Scripts/UIControl/UICharacterInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICharacterInfo : MonoBehaviour
+{
+    public GameObject CharacterInfo;
+    public Image Arrow;
+
+    bool IsOpened;
+
+    private void Start()
+    {
+        IsOpened = false;
+    }
+
+    public void OpenCloseCharacterInfo()
+    {
+        if (IsOpened)
+        {
+            CharacterInfo.GetComponent<RectTransform>().anchoredPosition = new Vector2(-69.5f, -59.75f); //나중에 애니메이션으로 부드럽게 만들 예정
+            Arrow.GetComponent<RectTransform>().rotation = new Quaternion(0, 0, 0, 0);
+            IsOpened = false;
+        }
+        else
+        {
+            CharacterInfo.GetComponent<RectTransform>().anchoredPosition = new Vector2(-1056.8f, -59.75f); //나중에 애니메이션으로 부드럽게 만들 예정
+            Arrow.GetComponent<RectTransform>().rotation = new Quaternion(0, 0, 180, 0);
+            IsOpened = true;
+        }
+            
+    }
+}
