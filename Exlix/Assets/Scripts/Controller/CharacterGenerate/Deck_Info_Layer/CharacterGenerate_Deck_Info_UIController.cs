50074f49addbe7ba7125094d3ef3601edc74f141
woohyun95 <diddngus77@naver.com>
2021-07-13 11:54:39 +0900

    날아간 씬 복구

--- Exlix/Assets/Scripts/Controller/CharacterGenerate/Deck_Info_Layer/CharacterGenerate_Deck_Info_UIController.cs
+++ Exlix/Assets/Scripts/Controller/CharacterGenerate/Deck_Info_Layer/CharacterGenerate_Deck_Info_UIController.cs
@@ -5,10 +5,12 @@ public class CharacterGenerate_Deck_Info_UIController : MonoBehaviour {
     [SerializeField] Text CardExplain;
     [SerializeField] Text CardMana;
     [SerializeField] Text CardTitle;
-    
+    [SerializeField] Text CardFaction;
+
     public void updateCardExplain(CardDTO card) {
         CardExplain.text = card.Explanation;
         CardMana.text = card.Mana.ToString();
         CardTitle.text = card.Title;
+        CardFaction.text = card.Property;
     }
 }
--- Exlix/Assets/Scripts/Object/CardSelection.cs
+++ Exlix/Assets/Scripts/Object/CardSelection.cs
@@ -5,20 +5,10 @@ using UnityEngine.EventSystems;
 
 public class CardSelection : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnPointerClick(PointerEventData eventData) {
         // 마우스 혹은 컨트롤러의 포인터로 버튼을 클릭했을 경우.
         Debug.Log(GetComponent<CardObject>().CardData.Explanation);
+
     }
 }
