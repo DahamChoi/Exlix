612114270f1109c1c70fd4cf8ed59baf91c4a8d5
woohyun95 <diddngus77@naver.com>
2021-07-14 16:04:20 +0900

    UIState 싱글톤 삭제

--- Exlix/Assets/Scripts/Controller/CharacterGenerate/Deck_Info_Layer/CharacterGenerate_Deck_Info_UIController.cs
+++ Exlix/Assets/Scripts/Controller/CharacterGenerate/Deck_Info_Layer/CharacterGenerate_Deck_Info_UIController.cs
@@ -9,7 +9,7 @@ public class CharacterGenerate_Deck_Info_UIController : MonoBehaviour, IPointerC
     [SerializeField] Text CardCost;
     [SerializeField] Text CardTitle;
     [SerializeField] Text CardFaction;
-
+    [SerializeField] UIState _UIState;
     [SerializeField] Canvas m_canvas;
     GraphicRaycaster m_gr;
     PointerEventData m_ped;
@@ -18,8 +18,6 @@ public class CharacterGenerate_Deck_Info_UIController : MonoBehaviour, IPointerC
         m_gr = m_canvas.GetComponent<GraphicRaycaster>();
         m_ped = new PointerEventData(null);
     }
-
-
 
     public void updateCardExplain(CardDTO card) {
         CardExplain.text = card.Explanation;
@@ -33,8 +31,8 @@ public class CharacterGenerate_Deck_Info_UIController : MonoBehaviour, IPointerC
         m_gr.Raycast(m_ped, results);
         if(results.Count > 0) {
             if (results[0].gameObject.transform.GetComponent<CardObject>()) {
-                UIState.GetInstance()._UIStateHandler.UpdateSelectedCard(results[0].gameObject.transform.GetComponent<CardObject>().CardData);
-                CardDTO card = UIState.GetInstance()._UIStateHandler.GetSelectedCard();
+                _UIState._UIStateHandler.UpdateSelectedCard(results[0].gameObject.transform.GetComponent<CardObject>().CardData);
+                CardDTO card = _UIState._UIStateHandler.GetSelectedCard();
                 CardExplain.text = card.Explanation;
                 CardCost.text = card.Cost.ToString();
                 CardTitle.text = card.Title;
--- Exlix/Assets/Scripts/State/UI/UIState.cs
+++ Exlix/Assets/Scripts/State/UI/UIState.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIState : Singleton<UIState> {
+public class UIState : MonoBehaviour {
     public UIStateHandler _UIStateHandler = new UIStateHandler();
 
     public void SubscribleUIStateInfo(IObserver<UIStateInfo> observer) {
