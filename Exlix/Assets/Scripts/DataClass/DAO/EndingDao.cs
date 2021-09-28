a62fed6a84a2dfae0445334d3708cdfa96afb609
ysh4267 <ysh4267@gmail.com>
2021-09-28 19:11:20 +0900

    Ending db, dao update

--- Exlix/Assets/Scripts/DataClass/DAO/EndingDao.cs
+++ Exlix/Assets/Scripts/DataClass/DAO/EndingDao.cs
@@ -7,19 +7,22 @@ public class EndingDao
     private static readonly string EndingTable = "ending";
     private static readonly string EndingTitleTable = "ending_title";
     private static readonly string EndingDescribeTable = "ending_describe";
+    private static readonly string IllustTable = "illust";
 
     public static Ending GetEnding() {
         string query =
-            $"SELECT" +
-            $"{EndingTable}.ending_index AS 'ending_index'," +
-            $"{EndingTitleTable}.ko_KR AS 'ending_title'," +
-            $"{EndingDescribeTable}.ko_KR AS 'ending_describe'," +
-            $"{EndingTable}.illust_index AS 'illust_index',"+
-            $"FROM {EndingTable}" +
-            $"LEFT JOIN {EndingTitleTable}" +
-            $"ON {EndingTable}.ending_index = {EndingTitleTable}.ending_index" +
-            $"LEFT JOIN {EndingDescribeTable}" +
-            $"ON {EndingTable}.ending_index = {EndingDescribeTable}.ending_index";
+            $"SELECT " +
+            $"{EndingTable}.ending_index AS 'ending_index', " +
+            $"{EndingTitleTable}.ko_KR AS 'ending_title', " +
+            $"{EndingDescribeTable}.ko_KR AS 'ending_describe', " +
+            $"{IllustTable}.image_path AS 'image_path'" +
+            $"FROM {EndingTable} " +
+            $"LEFT JOIN {EndingTitleTable} " +
+            $"ON {EndingTable}.ending_index = {EndingTitleTable}.ending_index " +
+            $"LEFT JOIN {EndingDescribeTable} " +
+            $"ON {EndingTable}.ending_index = {EndingDescribeTable}.ending_index " +
+            $"LEFT JOIN {IllustTable} " +
+            $"ON {EndingTable}.illust_index = {IllustTable}.illust_index";
 
         ExdioDataReader it = SQLiteManager.GetInstance().SelectQuery(query);
         
@@ -32,13 +35,7 @@ public class EndingDao
         ending.endingIndex = it.GetSafeValue<int>(1);
         ending.endingTitle = it.GetSafeValue<string>(2);
         ending.endingDescribe = it.GetSafeValue<string>(3);
-        
-        Illustration endingIllust = new Illustration();
-        int illustIndex = it.GetSafeValue<int>(4);
-
-        endingIllust = IllustrationDao.GetIllust(illustIndex);
-
-        ending.illust = endingIllust;
+        ending.illust.imagePath = it.GetSafeValue<string>(4);
 
         return ending;
     }
