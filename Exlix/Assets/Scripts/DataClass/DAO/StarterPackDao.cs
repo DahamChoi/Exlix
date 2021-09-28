b4043b58cad4a0b719f0c78df11ae16a604947d5
ysh4267 <ysh4267@gmail.com>
2021-09-28 18:15:13 +0900

    starterPack Query edit

--- Exlix/Assets/Scripts/DataClass/DAO/StarterPackDao.cs
+++ Exlix/Assets/Scripts/DataClass/DAO/StarterPackDao.cs
@@ -7,27 +7,30 @@ public class StarterPackDao
 {
     private static readonly string StarterPackTable = "starter_pack";
     private static readonly string StarterPackName ="starter_pack_name";
-    private static readonly string StarterPackDescribe = "starter_pack_descirbe";
+    private static readonly string StarterPackDescribe = "starter_pack_describe";
     private static readonly string StarterPackRequire = "starter_pack_req";
     private static readonly string StarterPackRequireDescribe = "starter_pack_req_describe";
+    private static readonly string IllustTable = "illust";
     public static StarterPack GetStarterPack(int index) {
-        string query = $"SELECT" +
-            $"{StarterPackTable}.starter_pack_index AS 'starter_pack_index'," +
-            $"{StarterPackTable}.card_list AS 'card_list'," +
-            $"{StarterPackName}.ko_KR AS 'name'," +
-            $"{StarterPackDescribe}.ko_KR AS 'describe'," +
-            $"{StarterPackTable}.image_path AS 'image_path'," +
-            $"{StarterPackRequire}.is_unlocked AS 'is_unlocked'," +
-            $"{StarterPackRequireDescribe}.ko_KR AS 'unlock_req_text'" +
-            $"FROM {StarterPackTable}" +
-            $"LEFT JOIN {StarterPackName}" +
-            $"ON {StarterPackTable}.starter_pack_index = {StarterPackName}.starter_pack_index" +
-            $"LEFT JOIN starter_pack_req" +
-            $"ON {StarterPackTable}.starter_pack_index = {StarterPackRequire}.starter_pack_index" +
-            $"LEFT JOIN starter_pack_describe" +
-            $"ON {StarterPackTable}.starter_pack_index = {StarterPackDescribe}.starter_pack_index" +
-            $"LEFT JOIN starter_pack_req_describe" +
-            $"ON {StarterPackTable}.starter_pack_index = {StarterPackRequireDescribe}.starter_pack_index"+
+        string query = $"SELECT " + 
+            $"{StarterPackTable}.starter_pack_index AS 'starter_pack_index', " + 
+            $"{StarterPackTable}.card_list AS 'card_list', " + 
+            $"{StarterPackName}.ko_KR AS 'name', " + 
+            $"{StarterPackDescribe}.ko_KR AS 'describe', " + 
+            $"{IllustTable}.image_path AS 'image_path'," + 
+            $"{StarterPackRequire}.is_unlocked AS 'is_unlocked', " + 
+            $"{StarterPackRequireDescribe}.ko_KR AS 'unlock_req_text' " + 
+            $"FROM {StarterPackTable} " + 
+            $"LEFT JOIN {StarterPackName} " + 
+            $"ON {StarterPackTable}.starter_pack_index = {StarterPackName}.starter_pack_index " + 
+            $"LEFT JOIN {StarterPackRequire} " + 
+            $"ON {StarterPackTable}.starter_pack_index = {StarterPackRequire}.starter_pack_index " + 
+            $"LEFT JOIN {StarterPackDescribe} " + 
+            $"ON {StarterPackTable}.starter_pack_index = {StarterPackDescribe}.starter_pack_index " + 
+            $"LEFT JOIN {StarterPackRequireDescribe} " + 
+            $"ON {StarterPackTable}.starter_pack_index = {StarterPackRequireDescribe}.starter_pack_index" + 
+            $"LEFT JOIN {IllustTable}" + 
+            $"ON {IllustTable}.illust_index = {IllustTable}.illust_index" + 
             $"WHERE {StarterPackTable}.starter_pack_index = {index}";
 
         StarterPack starterPack = new StarterPack();
